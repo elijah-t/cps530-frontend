694a1818fe4295534df66188371529ab1a95a47a
elijah-t <elijah.tungul@ryerson.ca>
2019-12-02 00:23:03 -0500

    Removed backend directory

--- backend/todo/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Todo #imports the created Todo class in "models.py" to be used here 
-
-class TodoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'completed')
-
-# Register your models here.
-admin.site.register(Todo, TodoAdmin)
--- backend/todo/serializers.py
+++
@@ -1,9 +0,0 @@
-#This file specifies the fields from the Todo model that we want to be converted into a JSON file for the front-end to take care of
-
-from rest_framework import serializers #may have to import from Todo instead
-from .models import Todo
-
-class TodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Todo
-        fields = ('id', 'title', 'description', 'completed')
